0638281f84f04c1aef925f850b74903c92efd857
Arahk Yambupah <arahk@outlook.com>
2022-09-20 18:40:56 +0700

    Make local rest call and use local db

--- Arahk.AntifakeNews.Infrastructure/Startup.cs
+++ Arahk.AntifakeNews.Infrastructure/Startup.cs
@@ -10,14 +10,14 @@ public static class Startup
     public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
         services.AddDbContext<DefaultDbContext>(options => {
-            options.UseInMemoryDatabase("Default");
-            //options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=AntifakeNews;Trusted_Connection=True;", b => b.MigrationsAssembly("Arahk.AntifakeNews.WebApi"));
+            //options.UseInMemoryDatabase("Default");
+            options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=AntifakeNews;Trusted_Connection=True;", b => b.MigrationsAssembly("Arahk.AntifakeNews.WebApi"));
         });
         services.AddScoped<IUnitOfWork, UnitOfWork>();
 
-        using var scope = services.BuildServiceProvider().CreateScope();
-        scope.ServiceProvider.GetRequiredService<DefaultDbContext>().Database.EnsureDeleted();
-        scope.ServiceProvider.GetRequiredService<DefaultDbContext>().Database.EnsureCreated();
+        // using var scope = services.BuildServiceProvider().CreateScope();
+        // scope.ServiceProvider.GetRequiredService<DefaultDbContext>().Database.EnsureDeleted();
+        // scope.ServiceProvider.GetRequiredService<DefaultDbContext>().Database.EnsureCreated();
 
         return services;
     }
